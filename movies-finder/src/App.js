21216473ea27b390f464a5cefa46ceee1f150124
amaan8429 <mohdamaan8429@gmail.com>
2023-08-24 12:54:26 +0530

    added a new project movies-finder

---
+++ movies-finder/src/App.js
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div className="app">
+      <div className="nav">
+        <div className="details">
+          <img src="/movie-icon.svg" alt="movie-icon" />
+          <div className="name">MovieFinder</div>
+        </div>
+        <div className="search">
+          <input />
+          <img src="/search-icon.svg" alt="search-icon" />
+        </div>
+      </div>
+    </div>
+  );
+}
